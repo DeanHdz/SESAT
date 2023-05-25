b86dae171d010d29d86493bdd737dfa9f82c51f8
BenjaminAlba <correopararoms@gmail.com>
2023-05-24 23:26:55 -0600

    update

--- sesat-frontend/src/Components/UserIcon/UserIcon.tsx
+++ sesat-frontend/src/Components/UserIcon/UserIcon.tsx
@@ -1,9 +1,18 @@
-const UserIcon = ({userName}:{userName:string}) =>
+import { useEffect, useState } from "react";
+import { ReactSession } from "react-client-session";
+
+const UserIcon = () =>
 {
+  ReactSession.setStoreType("sessionStorage");
+  const[usuario,setUsuario] = useState(sessionStorage.get("loggedUser"));
+
+
+
+
   return(
     <label tabIndex={0} className="btn btn-ghost btn-circle avatar placeholder">
       <div className="bg-[#31325c] text-neutral-content rounded-full w-[40px]">
-        <span className="text-xl">{userName}</span>
+        <span className="text-xl">{usuario.nombre}</span>
       </div>
     </label>
   )
