78b27caa7209aab4e23f3ad69fb2fbfa3380f0d3
thewolf003 <thewolf0310@outlook.com>
2023-09-12 18:04:19 -0600

    Archivos duplicados asesor

--- sesat-frontend/src/app/(admin)/admin-dashboard/sesat-users/alumnos/masters-degree/remove/page.tsx
+++ sesat-frontend/src/app/(admin)/admin-dashboard/sesat-users/alumnos/masters-degree/remove/page.tsx
@@ -1,23 +1,52 @@
 "use client";
+
+import React from 'react'
 import { useEffect, useState, ChangeEvent } from "react";
-import { Usuario } from "../../../types/ISESAT";
+import { Usuario } from '../../../../../../../../types/ISESAT';
 
 
+const RemoveAsesorForm = () => {
 
+  const [cssTab0, setCssTab0] = useState("");
+  const [cssTab1, setCssTab1] = useState("");
+  const [cssTab2, setCssTab2] = useState("tab-active");
 
-const UsersProfileList = () => {
-
-  const [users, setUsers] = useState<Usuario[]>();
+  const [hasShownUserInfo, setHasShownUserInfo] = useState(false);
   const [search, setSearch] = useState("");
-  const [selectedUser, setSelectedUser] = useState<Usuario | null>(null);
-  const [usuariosPrueba, setUsuariosPrueba] = useState<
+  const [users, setUsers] = useState<
     Usuario[] | undefined
   >();
-  let nombre = "";
-  const [cssTab0, setCssTab0] = useState("tab-active");
-  const [cssTab1, setCssTab1] = useState("");
-  const [cssTab2, setCssTab2] = useState("");
 
+
+  const [selectedUser, setSelectedUser] = useState<Usuario | null>(null);
+
+  async function getUsuarios() {
+    const token = "";
+    const url = `${process.env.NEXT_PUBLIC_SESAT_API_URL}/usuarios-prueba/`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, options);
+
+    setUsers(
+      await response.json()
+    );
+    setHasShownUserInfo(true);
+
+  };
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   function setActiveTab(tab: number) {
     switch (tab) {
@@ -39,28 +68,17 @@ const UsersProfileList = () => {
         break;
     }
   }
-
-  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  useEffect(() => {
-
-  }, []);
-
   return (
+    <form className="w-full flex flex-col">
 
 
-    <div className="w-full flex flex-col">
-
-      <label className="mb-6 block text-4xl font-bold">Asesores</label>
+      <label className="mb-6 block text-4xl font-bold">Alumnos de maestría</label>
 
       <div className="tabs">
-        <a href="/admin-dashboard/sesat-users/asesores" className={`tab tab-lifted ${cssTab0}`} onClick={() => { setActiveTab(1) }}>Asesores registrados en SESAT</a>
-        <a href="/admin-dashboard/sesat-users/asesores/register" className={`tab tab-lifted ${cssTab1}`} onClick={() => { setActiveTab(2) }}>Registrar asesor</a>
-        <a href="/admin-dashboard/sesat-users/asesores/remove" className={`tab tab-lifted ${cssTab2}`} onClick={() => { setActiveTab(3) }}>Baja de asesor</a>
+        <a href="/admin-dashboard/sesat-users/alumnos/masters-degree" className={`tab tab-lifted ${cssTab0}`} onClick={() => { setActiveTab(1) }}>Alumnos de maestría registrados en SESAT</a>
+        <a href="/admin-dashboard/sesat-users/alumnos/masters-degree/register" className={`tab tab-lifted ${cssTab1}`} onClick={() => { setActiveTab(2) }}>Registrar alumno</a>
+        <a href="/admin-dashboard/sesat-users/alumnos/masters-degree/remove" className={`tab tab-lifted ${cssTab2}`} onClick={() => { setActiveTab(3) }}>Baja de alumno</a>
       </div>
-
 
 
       <div className="mt-6 mb-6 p-2 border-t border-b border-light-gray-22 border-solid w-full flex justify-end">
@@ -78,6 +96,7 @@ const UsersProfileList = () => {
           <svg stroke="#d5d3dd" fill="#d5d3dd" strokeWidth="0" viewBox="0 0 24 24" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg"><path d="M19.023,16.977c-0.513-0.488-1.004-0.997-1.367-1.384c-0.372-0.378-0.596-0.653-0.596-0.653l-2.8-1.337 C15.34,12.37,16,10.763,16,9c0-3.859-3.14-7-7-7S2,5.141,2,9s3.14,7,7,7c1.763,0,3.37-0.66,4.603-1.739l1.337,2.8 c0,0,0.275,0.224,0.653,0.596c0.387,0.363,0.896,0.854,1.384,1.367c0.494,0.506,0.988,1.012,1.358,1.392 c0.362,0.388,0.604,0.646,0.604,0.646l2.121-2.121c0,0-0.258-0.242-0.646-0.604C20.035,17.965,19.529,17.471,19.023,16.977z M9,14 c-2.757,0-5-2.243-5-5s2.243-5,5-5s5,2.243,5,5S11.757,14,9,14z"></path></svg>
         </div>
       </div>
+
       <div className='mt-6 bg-light-blue-15 gray__border p-3'>
         <table className="table">
           {/*Table head */}
@@ -122,25 +141,15 @@ const UsersProfileList = () => {
         </table>
       </div>
 
-    </div>
-  );
 
-};
 
-/* 
-This code was already commented
-if (users)
-      for (let i = 0; i < users.length; i++) {
-        nombre =
-          users[i].nombre +
-          " " +
-          users[i].apellido_paterno +
-          " " +
-          users[i].apellido_materno;
-        if (
-          parseInt(search) == users[i].clave ||
-          nombre.toLowerCase().includes(search.toLowerCase())
-        )
-*/
+      <div className="w-full mt-6 flex justify-end">
+        <button type="submit" className="btn shadow rounded">
+          Dar de baja del sistema SESAT
+        </button>
+      </div>
+    </form>
+  )
+}
 
-export default UsersProfileList;
+export default RemoveAsesorForm
--- sesat-frontend/src/app/(admin)/admin-dashboard/sesat-users/asesores/register/page.tsx
+++ sesat-frontend/src/app/(admin)/admin-dashboard/sesat-users/asesores/register/page.tsx
@@ -1,12 +1,278 @@
 "use client";
-import AsesorRegistryForm from "@/app/components/AsesorRegistryForm";
-import ExternalAsesorRegistryForm from "@/app/components/ExternalAsesorRegistryForm";
+"use client";
+import { useState } from "react";
+import { UsuarioPrueba } from "../../../../../../../types/ISESAT";
 
 
-export default function Home() {
+
+
+export const AsesorRegistryForm = () => {
+  {/**Navegacion de pestañas */ }
+  const [cssTab0, setCssTab0] = useState("");
+  const [cssTab1, setCssTab1] = useState("tab-active");
+  const [cssTab2, setCssTab2] = useState("");
+  const [userType, setUserType] = useState("");
+
+
+  {/**Tabla de datos de asesores para seleccionar el que se va a registrar*/ }
+  const [selectedUser, setSelectedUser] = useState<UsuarioPrueba | null>(null);
+
+  const [claveUnica, setClaveUnica] = useState("");
+
+  const [hasShownUserInfo, setHasShownUserInfo] = useState(false);
+  const [usuariosPrueba, setUsuariosPrueba] = useState<
+    UsuarioPrueba[] | undefined
+  >();
+
+
+  
+
+
+
+  function setActiveTab(tab: number) {
+    switch (tab) {
+      case 1:
+        setCssTab0("tab-active");
+        setCssTab1("");
+        setCssTab2("");
+        break;
+      case 2:
+        setCssTab0("");
+        setCssTab1("tab-active");
+        setCssTab2("");
+        break;
+
+      default:
+        setCssTab0("");
+        setCssTab1("");
+        setCssTab2("tab-active");
+        break;
+    }
+  };
+
+  const handleUserTypeChange = (event: any) => {
+    setUserType(event.target.value);    
+  };
+  
+
+
+  {/** Commented for testing ui
+
+  const getUsuarioPrueba = async () => {
+    if (claveUnica && claveUnica.length == 6) {
+      setUsuarioPrueba(
+        await UsuarioPruebaEndpoint.getUsuarioPrueba(parseInt(claveUnica), "")
+      );
+      setHasShownUserInfo(true);
+    }
+
+    if (hasShownUserInfo == true) {
+      setUsuarioPrueba(
+        await UsuarioPruebaEndpoint.getUsuarioPrueba(parseInt(claveUnica), "")
+      );
+      setHasShownUserInfo(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsuarioPrueba();
+  }, [claveUnica]);
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    try {
+      await UsuarioEndpoint.postUsuario(
+        {
+          clave: parseInt(claveUnica),
+          nombre: usuarioPrueba?.nombre! ?? "",
+          apellido_paterno: usuarioPrueba?.apellido_pat! ?? "",
+          apellido_materno: usuarioPrueba?.apellido_mat! ?? "",
+          password: usuarioPrueba?.password! ?? "",
+          id_rol: 2,
+          id_datos_alumno: null,
+          correo: usuarioPrueba?.correo! ?? "",
+          id_datos_asesorexterno: null,
+        },
+        ""
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    //window.location.reload();
+  }
+ */}
+  //0 --> Medio tiempo
+  //1 -->Tiempo completo
+
   return (
-    <main>
-      <AsesorRegistryForm />      
-    </main>
-  )
-}
+    <>
+      <form className="w-full flex flex-col">
+
+
+        <label className="mb-6 block text-4xl font-bold">Asesores</label>
+
+        <div className="tabs">
+          <a href="/admin-dashboard/sesat-users/asesores" className={`tab tab-lifted ${cssTab0}`} onClick={() => { setActiveTab(1) }}>Asesores registrados en SESAT</a>
+          <a href="/admin-dashboard/sesat-users/asesores/register" className={`tab tab-lifted ${cssTab1}`} onClick={() => { setActiveTab(2) }}>Registrar asesor</a>
+          <a href="/admin-dashboard/sesat-users/asesores/remove" className={`tab tab-lifted ${cssTab2}`} onClick={() => { setActiveTab(3) }}>Baja de asesor</a>
+        </div>
+
+        <div className="h-fit w-full gray__border mt-10 p-6">
+          <span className="font-bold ">Complete los siguientes datos para realizar el registro.</span>
+          <div className="mt-6 flex lg:items-center flex-col lg:flex-row w-full h-fit">
+            <span>¿El asesor es actualmente profesor del área de posgrados?</span>
+            <select onChange={handleUserTypeChange} className="select select-bordered max-w-sm lg:ml-3 mt-3">
+              <option value="" disabled selected>Seleccione</option>
+              <option value={"asesor"} >Sí</option>
+              <option value={"externo"}>No, pertenece a otra institución/organización</option>
+            </select>
+          </div>
+
+          {userType === 'asesor' && (
+            <div className={`h-fit w-full my-10`}>
+              <div className="flex flex-col mt-10">
+                <span className="mt-3">Ingrese la clave única o el nombre asesor para buscar, seleccione una opción.</span>
+                <div className="mt-6">
+
+
+                  <input
+                    type="text"
+                    placeholder="Nombre o clave única"
+                    maxLength={6}
+                    className="gray__border input-bordered w-full max-w-lg"
+                    value={claveUnica}
+                    onChange={(e) => {
+                      setClaveUnica(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="mt-6 bg-light-blue-15 gray__border p-3">
+                <div className="overflow-x-auto">
+
+                  <table className="table">
+                    {/*Table head */}
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>Clave Única</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {usuariosPrueba?.map((user) => (
+                        <tr
+                          key={user.clave_unica}
+                          onClick={() => setSelectedUser(user)}
+                          /** Visualizar la seleccion de un renglon*/
+                          className={selectedUser?.clave_unica === user.clave_unica ? 'bg-white cursor-pointer' : 'cursor-pointer'}
+                        >
+                          <td>{user.clave_unica}</td>
+                          <td>{`${user.nombre} ${user.apellido_pat} ${user.apellido_mat}`}</td>
+                          <td>{user.correo}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  
+                </div>
+              </div>
+            </div>
+          )}
+
+
+
+          {userType === 'externo' && (
+            <div className={`h-fit w-full my-10`}>
+              <span className="mt-3">Ingrese los siguientes datos del asesor.</span>
+
+              <div className="flex flex-col lg:flex-row w-full lg:items-center my-6">
+                <label className="label">
+                  <span className="label-text">Nombre</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Nombre"
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+                <label className="lg:ml-6 label">
+                  <span className="label-text">Apellido Paterno</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Apellido Paterno"
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+              </div>
+
+              <div className="flex flex-col lg:flex-row w-full lg:items-center my-6">
+                <label className="label">
+                  <span className="label-text">Apellido Materno</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Apellido Materno"
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+
+                <label className="lg:ml-6 label">
+                  <span className="label-text">Teléfono</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Número de teléfono a 10 dígitos"
+                  maxLength={10}
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+              </div>
+
+              <div className="flex flex-col lg:flex-row w-full lg:items-center my-6">
+                <label className="label">
+                  <span className="label-text">Correo</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Correo"
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+                <label className="lg:ml-6 label">
+                  <span className="label-text">Institución/Organización</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Nombre"
+                  className="gray__border input-bordered w-full lg:max-w-[250px]"
+
+                />
+              </div>
+
+            </div>
+          )}
+
+
+
+          <div className="w-full mt-6 flex justify-end">
+            <button type="submit" className="btn shadow rounded">
+              Registrar asesor en SESAT
+            </button>
+          </div>
+
+        </div>
+
+
+
+
+
+      </form>
+    </>
+  );
+};
+
+export default AsesorRegistryForm;
