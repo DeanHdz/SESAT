ccf84abbff45b997cbffc2a3406c74d08fe0c17c
thewolf003 <thewolf0310@outlook.com>
2023-09-13 16:51:58 -0600

    Button CSS for SESAT

--- sesat-frontend/src/app/components/StudentProfileModal.tsx
+++ sesat-frontend/src/app/components/StudentProfileModal.tsx
@@ -10,9 +10,8 @@ const StudentProfileModal = ({ user }: { user: Usuario }) => {
   const [activeStatus, setActiveStatus] = useState<boolean>(true); //had to default it
   const [program, setProgram] = useState<string>("");
 
-  const [cssTab0, setCssTab0] = useState("tab-active");
-  const [cssTab1, setCssTab1] = useState("");
-  const [cssTab2, setCssTab2] = useState("");
+  const [activeTab0, setActiveTab0] = useState("tab-active");
+  const [activeTab1, setActiveTab1] = useState("");
 
 
   const [isChecked, setIsChecked] = useState(false);
@@ -31,21 +30,14 @@ const StudentProfileModal = ({ user }: { user: Usuario }) => {
   function setActiveTab(tab: number) {
     switch (tab) {
       case 1:
-        setCssTab0("tab-active");
-        setCssTab1("");
-        setCssTab2("");
+        setActiveTab0("tab-active");
+        setActiveTab1("");
         break;
       case 2:
-        setCssTab0("");
-        setCssTab1("tab-active");
-        setCssTab2("");
+        setActiveTab0("");
+        setActiveTab1("tab-active");
         break;
 
-      default:
-        setCssTab0("");
-        setCssTab1("");
-        setCssTab2("tab-active");
-        break;
     }
   }
   useEffect(() => {
@@ -126,21 +118,21 @@ const StudentProfileModal = ({ user }: { user: Usuario }) => {
   return (
     <>
       <button
-        className="bg-[SESAT] bg-dark-blue-10 text-white active:bg-blue-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-dark-blue-10 text-white hover:bg-dark-blue-10 font-normal text-sm px-5 py-1 rounded-full shadow hover:shadow-lg mr-1 mb-1 outline-none focus:outline-none ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
-        Editar Alumno
+        Modificar
       </button>
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-[600px] my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">                
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                <div className="flex items-start justify-between p-5 ">
                   <h3 className="text-2xl font-semibold">Editar datos del Alumno</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -152,98 +144,211 @@ const StudentProfileModal = ({ user }: { user: Usuario }) => {
                   </button>
                 </div>
 
-                <div className="tabs">
-                  <a href="/admin-dashboard/sesat-users/alumnos/masters-degree" className={`tab tab-lifted ${cssTab0}`} onClick={() => { setActiveTab(1) }}>Datos generales</a>
-                  <a href="/admin-dashboard/sesat-users/alumnos/masters-degree/register" className={`tab tab-lifted ${cssTab1}`} onClick={() => { setActiveTab(2) }}>Comité</a>
-                  <a href="/admin-dashboard/sesat-users/alumnos/masters-degree/remove" className={`tab tab-lifted ${cssTab2}`} onClick={() => { setActiveTab(3) }}></a>
+                <div className="tabs w-full px-6">
+                  <a className={`tab tab-lifted ${activeTab0}`} onClick={() => { setActiveTab(1) }}>Datos generales</a>
+                  <a className={`tab tab-lifted ${activeTab1}`} onClick={() => { setActiveTab(2) }}>Comité de evaluación</a>
                 </div>
 
-                <form onSubmit={handleSubmit}>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <div className="form-control w-full max-w-xs">
-                      <label className="label">
-                        <span className="my-3 block text-lg font-bold">
-                          Programa
-                        </span>
-                      </label>
-                      <select
-                        required
-                        className="select select-bordered rounded"
-                        onChange={(e) => {
-                          setProgram(e.target.value);
-                        }}
-                      >
-                        <option disabled selected>
-                          Seleccione una opción
-                        </option>
-                        {listaProgramas ? (
-                          <SelectProgramas programas={listaProgramas} />
-                        ) : (
-                          ""
-                        )}
-                      </select>
+                {activeTab0 === 'tab-active' && (
+                  <form onSubmit={handleSubmit}>
+                    {/*body*/}
+                    <div className="relative p-6 flex-auto">
+                      <div className="form-control w-full max-w-xs">
+                        <label className="my-3 block text-lg font-bold">
+                          Programa de posgrado
+                        </label>
+
+                        <select
+                          required
+                          className="select-bordered gray__border text-sm"
+                          onChange={(e) => {
+                            setProgram(e.target.value);
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar programa
+                          </option>
+                          {listaProgramas ? (
+                            <SelectProgramas programas={listaProgramas} />
+                          ) : (
+                            ""
+                          )}
+                        </select>
+                      </div>
+                      <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                          <span className=" my-3 block text-lg font-bold">
+                            Estado Activo
+                          </span>
+                        </label>
+                        <select
+                          required
+                          className="select-bordered gray__border text-sm"
+                          onChange={(e) => {
+                            if (e.target.value == "0") setActiveStatus(false);
+                            else if (e.target.value == "1") {
+                              setActiveStatus(true);
+                            }
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar estado activo
+                          </option>
+                          <option value={"0"}>Inactivo</option>
+                          <option value={"1"}>Activo</option>
+                        </select>
+                      </div>
+
                     </div>
-                    <div className="form-control w-full max-w-xs">
-                      <label className="label">
-                        <span className=" my-3 block text-lg font-bold">
-                          Estado Activo
-                        </span>
-                      </label>
-                      <select
-                        required
-                        className="select select-bordered rounded"
-                        onChange={(e) => {
-                          if (e.target.value == "0") setActiveStatus(false);
-                          else if (e.target.value == "1") {
-                            setActiveStatus(true);
+                    {/*footer*/}
+                    <div className="flex items-center justify-end px-6 py-2">
+                      <button
+                        className="secondary__btn mr-3 "
+                        type="button"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Descartar
+                      </button>
+                      <button
+                        className="primary__btn"
+                        type="submit"
+                      >
+                        Guardar Cambios
+                      </button>
+                    </div>
+                  </form>
+                )}
+                {activeTab1 === 'tab-active' && (
+                  <form onSubmit={handleSubmit}>
+                    {/*body*/}
+                    <div className="relative px-6 flex-auto">
+                      <div className="form-control w-full max-w-xs">
+
+                        <label className="my-3 block text-lg font-bold">
+                          Asesor de tesis
+                        </label>
+
+                        <select
+                          required
+                          className="select-bordered gray__border text-sm"
+                          onChange={(e) => {
+                            setProgram(e.target.value);
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar asesor
+                          </option>
+                          {listaProgramas ? (
+                            <SelectProgramas programas={listaProgramas} />
+                          ) : (
+                            ""
+                          )}
+                        </select>
+                      </div>
+                      <div className="form-control w-full max-w-xs">
+
+                        <label className=" my-2 block text-lg font-bold">
+                          Integrante 2
+                        </label>
+
+                        <select
+                          required
+                          className="select-bordered gray__border text-sm"
+                          onChange={(e) => {
+                            if (e.target.value == "0") setActiveStatus(false);
+                            else if (e.target.value == "1") {
+                              setActiveStatus(true);
+                            }
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar
+                          </option>
+                          <option value={"0"}>Inactivo</option>
+                          <option value={"1"}>Activo</option>
+                        </select>
+                      </div>
+
+                      <div className="form-control w-full max-w-xs">
+                        <label className="my-3 block text-lg font-bold">
+                          Integrante 3
+                        </label>
+                        <select
+                          className="select-bordered gray__border text-sm"
+                          required
+                          onChange={(e) => {
+                            setAsesor(e.target.value);
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar
+                          </option>
+                          {
+                            renderSelectAsesores() //display las opciones de asesores de manera condicional
                           }
-                        }}
-                      >
-                        <option disabled selected>
-                          Seleccione una opción
-                        </option>
-                        <option value={"0"}>Inactivo</option>
-                        <option value={"1"}>Activo</option>
-                      </select>
+                        </select>
+                      </div>
+                      <div className="form-control w-full max-w-xs">
+                        <label className="my-3 block text-lg font-bold">
+                          Integrante 4
+                        </label>
+                        <select
+                          className="select-bordered gray__border text-sm"
+                          required
+                          onChange={(e) => {
+                            setAsesor(e.target.value);
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar
+                          </option>
+                          {
+                            renderSelectAsesores() //display las opciones de asesores de manera condicional
+                          }
+                        </select>
+                      </div>
+
+                      <div className="form-control w-full max-w-xs">
+                        <label className="my-3 block text-lg font-bold">
+                          Integrante 5
+                        </label>
+                        <select
+                          className="select-bordered gray__border text-sm"
+                          required
+                          onChange={(e) => {
+                            setAsesor(e.target.value);
+                          }}
+                        >
+                          <option disabled selected>
+                            Actualizar
+                          </option>
+                          {
+                            renderSelectAsesores() //display las opciones de asesores de manera condicional
+                          }
+                        </select>
+                      </div>
+
                     </div>
-                    <div className="form-control w-full max-w-xs">
-                      <label className="my-3 block text-lg font-bold">
-                        Asesor
-                      </label>
-                      <select
-                        className="select select-bordered rounded"
-                        required
-                        onChange={(e) => {
-                          setAsesor(e.target.value);
-                        }}
+
+                    {/*footer*/}
+                    <div className="flex items-center justify-end px-6 py-2">
+                      <button
+                        className="secondary__btn mr-3 "
+                        type="button"
+                        onClick={() => setShowModal(false)}
                       >
-                        <option disabled selected>
-                          Nombre completo Asesor/a
-                        </option>
-                        {
-                          renderSelectAsesores() //display las opciones de asesores de manera condicional
-                        }
-                      </select>
+                        Descartar
+                      </button>
+                      <button
+                        className="primary__btn"
+                        type="submit"
+                      >
+                        Guardar Cambios
+                      </button>
                     </div>
-                  </div>
-                  {/*footer*/}
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                    <button
-                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Cerrar
-                    </button>
-                    <button
-                      className="bg-dark-blue-10 text-white active:bg-blue-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="submit"
-                    >
-                      Guardar Cambios
-                    </button>
-                  </div>
-                </form>
+                  </form>
+                )}
+
               </div>
             </div>
           </div>
