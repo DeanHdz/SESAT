d6b2bd11a69c2fb6d4d4d861969edf9c7b77460e
DeanHdz <dean2103@gmail.com>
2023-10-06 17:08:42 -0600

    Inicio creacion asignacion(asesor)

---
+++ sesat-frontend/src/app/(asesor)/asesor-assignment/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import AssignmentPage from "@/app/components/AssignmentPage"
+import AssignmentCard from "@/app/components/AssignmentCard"
+
+export default function Home() {
+  return (
+    <main className="">
+      <AssignmentCard />
+    </main>
+  )
+}
