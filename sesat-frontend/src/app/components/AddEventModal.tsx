314016473a64aeba5a509193a94e68d16c796115
BenjaminAlba <correopararoms@gmail.com>
2023-11-07 11:12:45 -0600

    Correct Time Validations in AddEventModal

--- sesat-frontend/src/app/components/AddEventModal.tsx
+++ sesat-frontend/src/app/components/AddEventModal.tsx
@@ -51,6 +51,8 @@ const AddEventModal = ({
   const [changeDate, setChangeDate] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean | undefined>(false);
 
+
+
   useEffect(() => {
     setStartDate(
       new Date(
@@ -406,6 +408,7 @@ const AddEventModal = ({
 
   const [showSuccessModal, setShowSuccessModal] = useState<boolean | undefined>(false);
   const [showErrorModal, setShowErrorModal] = useState<boolean | undefined>(false);
+  const [showErrorModal2, setShowErrorModal2] = useState<boolean | undefined>(false);
 
   const successModal = (
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -501,6 +504,55 @@ const AddEventModal = ({
     </div>
   );
 
+  const errorModal2 = (
+    <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                    className="h-6 w-6 text-red-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                    />
+                  </svg>
+                </div>
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Operación fallida</h3>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">La fecha de fin debe ser distinta a 00:00:00</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"> 
+              <a
+                href="/admin-dashboard"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                onClick={() => {
+                  router.refresh();
+                }}
+              >
+                Cerrar
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   const modal = (
     <div
       className="relative z-10"
@@ -538,15 +590,30 @@ const AddEventModal = ({
     </div>
   );
 
+  useEffect(() => {
+    if (start) {
+      const newEndDate = new Date(start.getTime() + 30 * 60000);
+      setEndDate(newEndDate);
+    }
+  }, [start]);
+
+  useEffect(() => {
+    if(end != undefined && end < start) {
+      const newEndDate = new Date(start.getTime() + 30 * 60000);
+      setEndDate(newEndDate);
+    }
+  }, [end]);
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     let evento = null;
+    let escape = 0;
     if (
       end?.getHours() == 0 &&
       end?.getMinutes() == 0 &&
       end?.getSeconds() == 0
     ) {
-      alert("Por favor ingrese una fecha de fin que no sea 00:00:00");
+      escape=1;
     }
     else if(btn1Active == false && btn2Active == false && participants.length == 0) {
       evento = await EventoEndpoint.postEvento(
@@ -605,7 +672,10 @@ const AddEventModal = ({
       setShowSuccessModal(!showSuccessModal);
     }
     else
-      setShowErrorModal(!showErrorModal);
+      if(escape== 0)
+        setShowErrorModal(!showErrorModal);
+      else
+        setShowErrorModal2(!showErrorModal2);
   };
 
   return (
@@ -624,6 +694,15 @@ const AddEventModal = ({
         (
           <>
             {errorModal}
+          </>
+        ) : (
+          ""
+        )
+      }
+      {showErrorModal2 ? 
+        (
+          <>
+            {errorModal2}
           </>
         ) : (
           ""
