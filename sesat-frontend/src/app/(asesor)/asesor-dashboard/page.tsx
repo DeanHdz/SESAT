3b74c61ea1fd0f99294e597c4ceed14a6bf370e5
Jesus <jesusgerardo.1315@hotmail.com>
2023-11-09 01:32:19 -0600

    asesor and alumno session, and notification fix

--- sesat-frontend/src/app/(asesor)/asesor-dashboard/page.tsx
+++ sesat-frontend/src/app/(asesor)/asesor-dashboard/page.tsx
@@ -29,14 +29,14 @@ export default async function Home() {
   const token: string = cookie ? cookie.substring(1, cookie?.length - 1) : "";
   const user: LoggedUser = await LoginEndpoint.getUserInfo(token);
 
-  const eventosData: Promise<Evento[]> = EventoEndpoint.getEventos("", user.id_usuario);
+  const eventosData: Promise<Evento[]> = EventoEndpoint.getEventos(token, user.id_usuario);
   const eventos = await eventosData;
   
   
-  let asesorID = 333333;
+  let asesorID = user.id_usuario;
   let idFuncion = 1;
-  let periodo = await fetchLatestPeriod("").catch();
-  let asignaciones: AsignacionProps[] = await fetchAvancesEntregados(periodo.id_periodo, asesorID.toString(), idFuncion.toString(), "")
+  let periodo = await fetchLatestPeriod(token).catch();
+  let asignaciones: AsignacionProps[] = await fetchAvancesEntregados(periodo.id_periodo, asesorID.toString(), idFuncion.toString(), token)
   return (
     <main className="w-full flex flex-row max-w-[1600px] mx-auto">
 
